a56baf4324eb27746815725ecdfe316e96920556
tfd-ed <info@tfdevs.com>
2025-07-16 10:28:34 +0700

    DATA

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import { users } from './sample.js';
 
 const app = express();
 
+/**
+ * Exercise : Create 5 resources
+ * GET /api/books
+ * GET /api/users
+ * GET /api/teachers
+ * GET /api/money
+ * GET /api/stock
+ */
+
 app.get('/users', (req, res) => {
-    return res.send('Hello World');
+    return res.json(users)
 })
 app.listen(3000, () => {
     console.log('Server runing on port 3000');
