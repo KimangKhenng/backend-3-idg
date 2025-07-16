fdf9522b72ee8c778586f3256a1a9a72d727f626
tfd-ed <info@tfdevs.com>
2025-07-16 11:01:19 +0700

    Get by ID

--- index.js
+++ index.js
@@ -6,15 +6,37 @@ const app = express();
 /**
  * Exercise : Create 5 resources
  * GET /api/books
+ * GET /api/books/{id}
+ * DELETE /api/books/{id}
  * GET /api/users
+ * GET /api/users/{id}
+ * DELETE /api/users/{id}
  * GET /api/teachers
+ * GET /api/teachers/{id}
+ * DELETE /api/teachers/{id}
  * GET /api/money
+ * GET /api/money/{id}
+ * DELETE /api/money/{id}
  * GET /api/stock
+ * GET /api/stock/{id}
+ * DELETE /api/stock/{id}
  */
 
-app.get('/users', (req, res) => {
+app.get('/api/users', (req, res) => {
     return res.json(users)
 })
+
+app.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users.find((u) => {
+        return u.id == id
+    })
+    if (!user) {
+        return res.json({ messsge: "Not Found" })
+    }
+    return res.json(user)
+})
+
 app.listen(3000, () => {
     console.log('Server runing on port 3000');
 })
