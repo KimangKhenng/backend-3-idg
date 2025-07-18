2224fac07df15e5bf3c33e71e9fefaaea37c196a
tfd-ed <info@tfdevs.com>
2025-07-18 11:14:04 +0700

    Remove Hello

--- src/index.js
+++ src/index.js
@@ -13,10 +13,6 @@ app.use('/api/users', userRoute);
 app.use('/api/teachers', teacherRoute);
 app.use('/api/stocks', stockRoute);
 
-app.get('/hello', (req, res) => {
-    return res.json("Hello")
-})
-
 app.listen(3000, () => {
     console.log('Server runing on port 3000');
 })
