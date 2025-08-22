84ffa0c2db24c321de316e0973af941ead0d0d7a
tfd-ed <info@tfdevs.com>
2025-08-22 14:25:35 +0700

    TEST

--- src/index.js
+++ src/index.js
@@ -34,28 +34,28 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/users',
-    limiter(60 * 1000, 30), // 1 minute, 30 requests
-    // authenticate,
+    // limiter(60 * 1000, 30), // 1 minute, 30 requests
+    authenticate,
     cacheMiddleware,
     CacheInterceptor(60 * 10),
     invalidateCache,
     userRoute);
 app.use('/api/teachers',
-    limiter(60 * 1000, 60), // 1 minute, 60ß requests
+    // limiter(60 * 1000, 60), // 1 minute, 60ß requests
     authenticate,
     cacheMiddleware,
     CacheInterceptor(60 * 10),
     invalidateCache,
     teacherRoute);
 app.use('/api/stocks',
-    limiter(60 * 1000, 30), // 1 minute, 30 requests
+    // limiter(60 * 1000, 30), // 1 minute, 30 requests
     authenticate,
     cacheMiddleware,
     CacheInterceptor(60 * 10),
     invalidateCache,
     stockRoute);
 app.use('/api/courses',
-    limiter(60 * 1000, 30), // 1 minute, 30 requests
+    // limiter(60 * 1000, 30), // 1 minute, 30 requests
     // authenticate,
     cacheMiddleware,
     CacheInterceptor(60 * 10),
@@ -66,7 +66,7 @@ app.use('/api/files', fileRoute);
 
 // Auth
 app.use('/api/auth',
-    limiter(60 * 60 * 1000, 3), // 1 hour, 3 requests
+    // limiter(60 * 60 * 1000, 3), // 1 hour, 3 requests
     authRoute);
 
 app.use(handleError)
